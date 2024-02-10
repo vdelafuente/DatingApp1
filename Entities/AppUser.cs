5621ba0e0424e5bd65aba4e3a8c99ff3ff60447c
Victor de la Fuente <victorxp@gmail.com>
2024-02-10 15:36:04 -0500

    first commit

---
+++ Entities/AppUser.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    // public byte[] PasswordHash { get; set; } = null!;
+    // public byte[] PasswordSalt { get; set; } = null!;
+    // public string? FirstName { get; set; }
+    // public string? LastName { get; set; }
+    // public string? Email { get; set; }
+    // public string? PhoneNumber { get; set; }
+    // public string? Address { get; set; }
+    // public string? City { get; set; }
+    // public string? Country { get; set; }
+    // public string? PostalCode { get; set; }
+    // public string? PhotoUrl { get; set; }
+    // public string? PhotoPublicId { get; set; }
+    // public DateTime Created { get; set; }
+    // public DateTime LastActive { get; set; }
+    // public bool IsActive { get; set; }
+    // public bool IsDeleted { get; set; }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+app.Run();
